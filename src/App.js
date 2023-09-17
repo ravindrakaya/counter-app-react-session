9d28961e6c187e0ffe263d11366c8ae6fabb2d50
ravindrakaya <kayaravindra118@gmail.com>
2023-09-17 10:46:28 +0000

    solution

---
+++ src/App.js
@@ -0,0 +1,15 @@
+/*
+import Welcome from './components/Welcome'
+
+const App = () => {
+  return <Welcome name="Rahul" />
+}
+export default App
+*/
+
+import Counter from './components/Counter'
+
+const App = () => {
+  return <Counter />
+}
+export default App
